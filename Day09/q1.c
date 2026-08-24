81831de76cc85af99b91490afbda9610c54170e2
Kanika Chaturvedi <kanikachaturvedi127@gmail.com>
2026-08-24 11:24:08 +0530

    working

--- Day09/q1.c
+++ Day09/q1.c
@@ -1,56 +1,59 @@
-//Q17: Write a program to find the roots of a quadratic equation and categorize them.
+/*Q18: Write a program that accepts a percentage (0-100) and assigns a grade based on the following criteria: 
+90-100: Grade A 
+80-89: Grade B 
+70-79: Grade C 
+60-69: Grade D 
+below 60: Grade F.
+*/
 
 /*
 Sample Test Cases:
 Input 1:
-1 -3 2
+95
 Output 1:
-Roots are real and different: 2, 1
+Grade A
 
 Input 2:
-1 -2 1
+82
 Output 2:
-Roots are real and same: 1
+Grade B
 
 Input 3:
-1 2 5
+68
 Output 3:
-Roots are complex
+Grade D
+
+Input 4:
+50
+Output 4:
+Grade F
 
 */
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-    float a, b, c, discriminant, root1, root2, realPart, imaginaryPart;
+    float percentage;
 
-    printf("Enter coefficients a, b and c: ");
-    scanf("%f %f %f", &a, &b, &c);
+    printf("Enter percentage: ");
+    scanf("%f", &percentage);
 
-    if (a == 0) {
-        printf("This is not a quadratic equation.\n");
-        return 0;
+    if (percentage < 0 || percentage > 100) {
+        printf("Invalid percentage\n");
     }
-
-    discriminant = b * b - 4 * a * c;
-
-    if (discriminant > 0) {
-        root1 = (-b + sqrt(discriminant)) / (2 * a);
-        root2 = (-b - sqrt(discriminant)) / (2 * a);
-
-        printf("Roots are real and different: %.2f, %.2f\n", root1, root2);
+    else if (percentage >= 90) {
+        printf("Grade A\n");
     }
-    else if (discriminant == 0) {
-        root1 = -b / (2 * a);
-
-        printf("Roots are real and same: %.2f\n", root1);
+    else if (percentage >= 80) {
+        printf("Grade B\n");
+    }
+    else if (percentage >= 70) {
+        printf("Grade C\n");
+    }
+    else if (percentage >= 60) {
+        printf("Grade D\n");
     }
     else {
-        realPart = -b / (2 * a);
-        imaginaryPart = sqrt(-discriminant) / (2 * a);
-
-        printf("Roots are complex\n");
-        printf("Roots: %.2f + %.2fi, %.2f - %.2fi\n",realPart, imaginaryPart, realPart, imaginaryPart);
+        printf("Grade F\n");
     }
 
     return 0;
